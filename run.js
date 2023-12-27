0fa741556265ca0fe23f6933cedbf0ee7b97b124
LLZ66 <1228429428@qq.com>
2023-12-27 22:06:04 +0800

    fix(*):33

--- run.js
+++ run.js
@@ -1,29 +1,13 @@
 const { exec } = require("child_process");
+const { stderr } = require("process");
 
 // 定义要检查的文件夹数组
 const folders = ["a", "b", "c", "d"];
 
 // 获取上一次提交的哈希值
-exec("git rev-parse HEAD^", (err, lastCommitHash) => {
-  if (err) {
-    console.error("Error getting last commit hash:", err);
-    return;
-  }
-
-  lastCommitHash = lastCommitHash.trim();
-
-  folders.forEach((folder) => {
-    // 对每个文件夹使用git diff检查变化
-    exec(
-      `git diff --quiet ${lastCommitHash} HEAD -- ${folder}`,
-      (err, stdout, stderr) => {
-        if (err) {
-          // 如果有错误，意味着发生了变化
-          console.log(`Changes detected in ${folder}`);
-        } else {
-          console.log(`No changes in ${folder}`);
-        }
-      }
-    );
+const REQUIRE_RE_NPM_INSTALL = false;
+for (let file of folders) {
+  exec('git log -1 --format="%H" ', (error, stdout, stderr) => {
+    console.log(stdout);
   });
-});
+}
